6d3319e6525d8bb75ee0bb3fcbcf97d666f956dd
ZaneN8 <zanelnewsom@gmail.com>
2020-08-26 16:47:30 -0600

    worked on items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,61 @@
 class ItemsController < ApplicationController
+  before_action :set_items  #, only: [:index, :show, :edit, :update, :destroy]
+
+
   def index
+    # @store = Store.find(params[:store_id])
+    @items = @store.items
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def new
+    @item = @store.item.new
+  end
+
+  def create
+    @item = @store.item.new(item_params)
+
+    if @item.save
+      redirect_item
+    else
+      render :new
+    end
   end
 
   def edit
+    @item = @store.item.find(params[:id])
+  end
+
+  def update
+    @item = @store.item.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_item
+    else
+      render :edit
+    end
+  end
+
+    def destroy
+      @item = @store.item.find(params[:id])
+      @item.destroy
+      redirect_item
+    end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
+
+  def set_items
+    @store = Store.find(params[:store_id])
+  end
+
+  def redirect_item
+    redirect_to store_item_path(@store, @item)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+3.times do |i|
+  store_seed = Store.create(name: Faker::Commerce.department,
+   description: Faker::Company.catch_phrase)
+end
+
+
+3.times do |i|
+  store_seed.items.create(name: Faker::Commerce.product_name,
+   description: Faker::Commerce.material, price: Faker::Commerce.price)
+end

d9c30c8c38e6acd70f38b6264666e90180719202
Jon Roberts <kewlguy@gmail.com>
2020-08-26 16:46:44 -0600

    added stores stuff, and fixed the routes

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,13 +1,57 @@
 class StoresController < ApplicationController
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @stores = Store.all
   end
 
   def show
+   # @store = Store.find(params[:id])
   end
 
   def new
+    @store = Store.new
+    @url = "/path"
+    @method  = :post
+    render partial: "form" 
+
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      redirect_to store_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
+  end
+
+ def update
+    if @store.update(store_params)
+      redirect_to store_path
+    else
+      render :edit
+    end
+  end
+
+def destroy
+  @store.destroy
+  redirect_to store_path
+end
+
+  private
+
+  def store_params
+    params.require(:store).permit(:name)
+  end
+
+  def set_store
+    @store = Store.find(params[:id])
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'stores#index'
 
-  resource :store do
-    resource :item
+  resources :stores do
+    resources :items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
